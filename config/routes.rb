2a397f8f28c4a9e1c7cf4a9c1d41ea1065c470f2
Gustavo Galeazzi <gustavogaleazzi@gmail.com>
2020-02-09 17:28:19 -0500

    MyFirstBlog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "/", to: 'main#index' #original was(get 'main/index')
-  
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'main/index'
+  resources :articles do
+    resources :comments
+  end
+ 
+  root 'main#index' 
 end
